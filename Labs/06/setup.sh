94e38e145bb68a4fcf485404ba6acfc1debd85d3
jurieen <jurieen@gmail.com>
2024-06-26 10:59:14 +0200

    Update setup.sh

--- Labs/06/setup.sh
+++ Labs/06/setup.sh
@@ -33,7 +33,7 @@ az ml compute create --name ${COMPUTE_INSTANCE} --size STANDARD_DS13_V2 --type C
 
 # Create compute cluster
 echo "Creating a compute cluster with name: " $COMPUTE_CLUSTER
-az ml compute create --name ${COMPUTE_CLUSTER} --size STANDARD_DS13_V2 --min-instances --max-instances 2 --type AmlCompute 
+az ml compute create --name ${COMPUTE_CLUSTER} --size STANDARD_DS13_V2 --min-instances 1 --max-instances 2 --type AmlCompute 
 
 # Create data assets
 echo "Create training data asset:"
